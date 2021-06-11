6e633182f657592b71d5f7f4e37022b4da8a1a32
Brandon Zirulnikoff <zirBrandonWeb@gmail.com>
2021-06-10 22:12:55 -0700

    Head style

--- src/components/FormProgress/FormProgress.jsx
+++ src/components/FormProgress/FormProgress.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
+import './formProgress.scss';
+
 const FormProgress = ({ authStep }) => {
   return (
     <div id="progress__container">
-      <span className="progress-text">
-        Step {authStep} of 3
-      </span>
-      
-      <ul>
-        <li style={{ opacity: authStep === 1 ? 1 : 0.4 }}></li>
-        <li style={{ opacity: authStep === 2 ? 1 : 0.4 }}></li>
-        <li style={{ opacity: authStep === 3 ? 1 : 0.4 }}></li>
-      </ul>
+      <div id="progress-content">
+        <span className="progress-text">
+          Step {authStep} of 3
+        </span>
+        
+        <ul>
+          <li style={{ opacity: authStep === 1 ? 1 : 0.4 }}></li>
+          <li style={{ opacity: authStep === 2 ? 1 : 0.4 }}></li>
+          <li style={{ opacity: authStep === 3 ? 1 : 0.4 }}></li>
+        </ul>
+      </div>
     </div>
   )
 }
